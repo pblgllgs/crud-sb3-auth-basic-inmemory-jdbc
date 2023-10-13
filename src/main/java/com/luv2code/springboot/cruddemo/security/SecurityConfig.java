9e6f8bacb07ce3a2c84941922cad38d5b6ed72c2
pbl.gllgs <pbl.gllgs@gmail.com>
2023-10-12 23:15:13 -0300

    jdbc authentication

--- src/main/java/com/luv2code/springboot/cruddemo/security/SecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/SecurityConfig.java
@@ -5,10 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -20,26 +21,8 @@ public class SecurityConfig {
 
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}john")
-                .roles(EMPLOYEE)
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}mary")
-                .roles(EMPLOYEE, MANAGER)
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}susan")
-                .roles(EMPLOYEE, MANAGER, ADMIN)
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
